f55745732b96937e933732438672109ce9c5473d
mru2 <david.ruyer+github@gmail.com>
2019-10-15 17:52:03 +0200

    Force synchro on jingles

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 #!/usr/bin/env node
 
-const Pusher = require("pusher-js/node");
-const execFile = require("child_process").execFile;
+const Pusher = require('pusher-js/node');
+const execFile = require('child_process').execFile;
 
-console.log("Hello world!");
+function buildQueue(handler) {
+  let running = false;
+  let queue = [];
 
-function handleNotify(data) {
-  console.log("Got notification", data);
-  const child = execFile("/opt/jingle/play-jingle.sh", [data.gif, data.time], (error, stdout, stderr) => {
-    if (error) {
-      throw error;
+  function popQueue() {
+    const e = queue.shift();
+    if (e) {
+      running = true;
+      handler(e, function() {
+        running = false;
+        popQueue();
+      });
     }
-    console.log(stdout);
-  });
+  }
+
+  return function(e) {
+    queue.push(e);
+    if (!running) {
+      popQueue();
+    }
+  };
 }
 
-const pusher = new Pusher("c4de1cc2f3f02653b55c", {
-  cluster: "eu",
-  encrypted: true
+function runJingle(data, cb) {
+  const child = execFile(
+    '/opt/jingle/play-jingle.sh',
+    [data.gif, data.time],
+    (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      console.log(stdout);
+      cb();
+    },
+  );
+}
+
+const enqueue = buildQueue(runJingle);
+
+const pusher = new Pusher('c4de1cc2f3f02653b55c', {
+  cluster: 'eu',
+  encrypted: true,
 });
 
-const channel = pusher.subscribe("tv");
+const channel = pusher.subscribe('tv');
 
-channel.bind("notify", handleNotify);
+channel.bind('notify', enqueue);
